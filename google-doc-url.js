602d5e96937221f39b3504a66468ac46bd575753
Marcel Dekker <38853712+zoefSmoelt@users.noreply.github.com>
2023-02-22 11:46:23 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://zoefsmoelt.github.io/thuiswonendeouderen/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/11jGtgChrtq7zvrreSyBcodx50mn8p6VK2XfP6fbzQA0/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyD4xPS646POtwvhHttYgcF23L-szIn_jHY';
